6360d520649276f2415f410077428aa8154ce182
CaioCeesaar <83191307+CaioCeesaar@users.noreply.github.com>
2023-11-24 03:05:02 -0300

    Subida

---
+++ AppClientes/Cliente.cs
@@ -0,0 +1,8 @@
+﻿public class Cliente
+{
+    public int Id  { get; set; }
+    public string Nome { get; set; }
+    public DateOnly DataNascimento { get; set; }
+    public DateTime CadastradoEm { get; set; }
+    public decimal Desconto { get; set; }
+}
